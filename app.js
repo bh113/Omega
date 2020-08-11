017c387040b9f404e7e9519856fa7d6257a61d67
bh113 <69528212+bh113@users.noreply.github.com>
2020-08-11 22:39:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyB1b3OxB6-1MxSjHh-MxGotB2ko0pr7Nz0",
+  authDomain: "instagram-fe0db.firebaseapp.com",
+  databaseURL: "https://instagram-fe0db.firebaseio.com",
+  projectId: "instagram-fe0db",
+  storageBucket: "instagram-fe0db.appspot.com",
+  messagingSenderId: "621896720289",
+  appId: "1:621896720289:web:47115e116928e23d12f5e0",
+  measurementId: "G-JNVKCH37LZ"
 };
 
      
